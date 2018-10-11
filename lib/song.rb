6d268fff25dad86831c066d1f2e2ed8cff7a9d7b
Veraprot <vera.protopopova@gmail.com>
2018-10-11 09:53:23 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+#song belong to artist 
+#post belong to author 
+#artist ---> name 
+#song ---> title, should be able to tell you a name of its artist 
+
+class Song 
+  attr_accessor :artist, :title 
+end 
+
+# song = song.new('this is tiiiitle')
+# artist = artist.new('vera'
+
+# song.artist = artist 
+# song.artist.name
